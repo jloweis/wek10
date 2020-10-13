a416bcf1c4279ed10f787f8bc1afcb68dc145fb7
jloweis <jaiden.oweis@students.mq.edu.au>
2020-10-13 16:55:46 +1100

    Chekc ncode
    reeeeeeeeeeeeeeeeeeeeeeeee

---
+++ Week 10 project/Assets/Scripts/SCoreKeeper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private int currentScore;
+    public int pointsPerCoin;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScore = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void IncrementScore()
+    {
+        currentScore += pointsPerCoin;
+    }
+}
