f74411102ec30b7e028deec18021666df8406a76
jloweis <jaiden.oweis@students.mq.edu.au>
2020-10-13 17:28:29 +1100

    step3
    reeeeeeeeee

--- Week 10 project/Assets/SCript/SCoreKeeper.cs
+++ Week 10 project/Assets/SCript/SCoreKeeper.cs
@@ -1,29 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SCoreKeeper : MonoBehaviour
 {
     private int currentScore;
+    public int CurrentScore
+    {
+        get
+        {
+            return(currentScore);
+        }
+    }
     public int pointsPerCoin;
-
+    public UIManager UIManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentScore = 0;
-
+       currentScore = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
-    void IncrementScore()
+    public void IncrementScore()
     {
         currentScore += pointsPerCoin;
+        UIManager.UpdateScore();
     }
 }
